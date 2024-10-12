ca4f33c137bbb0b88745caf46fb42b63d898e998
seu nome aqui <carloscelestino93@gmail.com>
2024-10-11 22:30:02 -0300

    Create aula1.py
    att

---
+++ dados_das_aulas/aula1.py
@@ -0,0 +1,14 @@
+
+
+nome = input("Digite seu nome: ")
+estrelas = input("Digite a quantidade de estrelas: ")
+cidade = input("Digite a cidade: ")
+
+print("*"*20)
+print('*'*5, nome, '*'*5)
+print(f'*'*4, estrelas, 'estrelas', '*'*4)
+print(f'*'*6, cidade, '*'*6)
+print("*"*20)
+
+
+
